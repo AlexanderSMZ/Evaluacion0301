aa621e6310c8744b71f0c26b931ac9b7b9e25220
alexa <alexa@DESKTOP-U11IB3O>
2022-06-17 07:03:57 -0500

    V1

---
+++ src/main/java/com/mycompany/evaluacion0301/Evaluacion0301.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.evaluacion0301;
+
+/**
+ *
+ * @author alexa
+ */
+public class Evaluacion0301 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
